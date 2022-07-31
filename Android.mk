d79905922c8d572d812d2273c538d52cc6e73855
Meesam <70945820+meesam4687@users.noreply.github.com>
2022-07-31 20:01:58 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 ifneq ($(filter a50cml_dtul,$(TARGET_DEVICE)),)
 
-LOCAL_PATH := device/htc/a50cml_dtul
+LOCAL_PATH := device/htc/htc_a50cml_dtul
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
